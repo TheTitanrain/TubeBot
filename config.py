649c3f3e3dc34c1bbcf62e9d3b607ab48e680c99
Alex <graymatter@list.ru>
2017-12-07 11:42:56 +1000

    Delete config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-DEBUG_ = True
-#debug bot token
-if DEBUG_:
-    token = '438008268:AAGsFVk_a5PENjEimW6sv-4NWaC0zLWovmo'
-else:
-    token = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
-    botan_key = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
-    # botan_key = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
-
